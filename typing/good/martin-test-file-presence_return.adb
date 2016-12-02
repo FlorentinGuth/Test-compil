94813bc9815345e324cb6233e39e1746b90b5115
MartinusR <martinruffel@free.fr>
2016-12-02 13:46:31 +0100

    Normalement les tests bizarres ont ete supprimes

--- typing/good/martin-test-file-presence_return.adb
+++
@@ -1,9 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-
-procedure presence_return is
-   function f (x : boolean)return integer is
-   	begin
-   	if (x or true) then return 1; end if;
-        end;
-
-begin new_line; End;
