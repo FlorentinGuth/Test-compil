d68d9428d4df43d6b84b88a7dc6425fc46e9e368
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2016-12-03 19:25:06 +0100

    modified shadCholera

--- typing/good/lionel_shadCholera.adb
+++ typing/good/lionel_shadCholera.adb
@@ -7,10 +7,13 @@ procedure Test is
    type boolean   is record i : access integer  ; end record;
    type integer   is record c : access character; end record;
    type character is record b : boolean         ; end record;
-   x : r; bo : boolean ; inte : integer ; ch : character;
+   x : r; 
+   bo : boolean ; 
+   bTmp : boolean := bo;
 begin
    x.int := 0 ; x.char := 'a' ; x.bool := false;
-   ch.b := bo ; 
-   inte.c.b := bo ; 
-   bo.i.c := new character;
+   for i in 0 .. 10 loop
+      bo.i.c.b := bTmp;
+      bTmp := bo;
+   end loop;
 end;
