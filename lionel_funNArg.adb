5c4e39799dfaaa8578af5a1e0a38049e282be486
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2016-12-03 18:14:29 +0100

    retour à la normale

--- lionel_funNArg.adb
+++
@@ -1,6 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-procedure Test is
-   function quatre return character is
-	x : character := 'd';
-	begin return x; end;
-begin put (quatre); end; -- quatre est une fonction qui renvoie un character
--- lionel_shadProcVar.adb
+++
@@ -1,9 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-
-procedure Test is
-   procedure X (y : integer) is 
-      x : character := 'a'; -- x cache la procédure
-   begin Put (x) ; end;
-begin
-   X(3);
-end;
