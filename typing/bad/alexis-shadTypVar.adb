3136746d7f2e0f8f6ccbcb7f52e0d21d8630eae9
Alexis Thibault <alexis.thibault1@gmail.com>
2016-12-06 17:12:54 +0100

    Encore un peu de shadowing. Inspired by Martin ;)

---
+++ typing/bad/alexis-shadTypVar.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure A is
+   type B is record
+      X : Integer;
+   end record;
+   procedure P is
+      B : B;
+   begin
+      New_Line;
+   end P;
+begin
+   New_Line;
+end;
